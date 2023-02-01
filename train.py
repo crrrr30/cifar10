c00466d2b745e84a539c2ec1c826c56666a8978c
crrrr30 <jonathancui03@gmail.com>
2023-01-31 20:26:28 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-from pytorch_lightning.tuner import lr_finder
-
 import argparse
 import numpy as np
 
@@ -19,7 +17,7 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description = 'Train CIFAR-10')
     
-    parser.add_argument('--num-epochs', help = 'Number of epochs to train the model for', default = 500, type = int)
+    parser.add_argument('--num-epochs', help = 'Number of epochs to train the model for', default = 2000, type = int)
     parser.add_argument('--batch-size', help = 'Batch size', default = 256, type = int)
     parser.add_argument('--log', help = 'Directory to save logs to', default = 'logs', type = str)
     parser.add_argument('--resume', help = 'Directory of the checkpoint to resume from or the path to the checkpoint', default = None, type = str)
@@ -30,44 +28,32 @@ def parse_args():
 
 
 class LitClassifier(pl.LightningModule):
-        def __init__(self, model_config, lr=1e-4):
+        def __init__(self, model_config, lr=1e-3):
             super().__init__()
             self.learning_rate = lr
-            self.automatic_optimization = False
             self.num_epochs = model_config["num_epochs"]
             self.model = FMLPS_4()
             self.criterion = nn.CrossEntropyLoss()
         def forward(self, x):
             return self.model(x)
         def configure_optimizers(self):
-            optimizer = Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=.1)
+            optimizer = Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=3e-2)
             scheduler = LinearLR(
                 optimizer,
                 start_factor=0.0015,
                 end_factor=0.0195,
                 total_iters=self.trainer.estimated_stepping_batches * self.num_epochs
             )
-            return [optimizer], [scheduler]
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": scheduler
+            }
         def training_step(self, data, idx):
-            optimizer = self.optimizers()
-            scheduler = self.lr_schedulers()
-            optimizer.zero_grad()
             x, y = data
             y_hat = self.model(x)
             loss = self.criterion(y_hat, y)
-            self.manual_backward(loss)
-            self.log("train_loss", loss, prog_bar=True)
-            norm = torch.nn.utils.clip_grad.clip_grad_norm(self.model.parameters(), max_norm=1.)
-            self.log("grad_norm", norm.item(), prog_bar=True)
-            optimizer.step()
-            scheduler.step()
-            return {
-                "loss": loss,
-                "progress_bar": {
-                    "loss": loss.item(),
-                    "grad_norm": norm.item(),
-                }
-            }
+            self.log("train_loss", loss, prog_bar=False)
+            return loss
         def validation_step(self, data, idx):
             x, y = data
             y_hat = self.model(x)
@@ -87,7 +73,7 @@ if __name__ == '__main__':
     train_dataloader = get_train_dataloader(args.batch_size)
     test_dataloader = get_test_dataloader(args.batch_size)
 
-    print('==> Building CNN...')
+    print('==> Building NN...')
     
     tb_logger = pl.loggers.TensorBoardLogger(".")
 
@@ -102,6 +88,9 @@ if __name__ == '__main__':
         logger=tb_logger,
         check_val_every_n_epoch=args.checkpoint_interval,
         precision=16 if args.half else 32,
+        accumulate_grad_batches=4096 // args.batch_size,
+        track_grad_norm=2,
+        gradient_clip_val=1.,
     )
 
     print("==> Optimizing LR")
